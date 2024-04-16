e72be8b7bcf56249b591b460bded9246282eeec1
lsx <omg.xiang@outlook.com>
2024-04-16 12:59:51 +0800

    fix

---
+++ lib/database/shared_preferences_table.dart
@@ -0,0 +1,29 @@
+
+/*
+* 使用 SharePreferences 模拟简单的数据库存储
+* */
+
+mixin SharePreferencesTable {
+
+  String get name;
+  String get key;
+
+
+  static all() {
+
+  }
+
+  static clear() {
+
+  }
+
+  static insert() {
+
+  }
+
+  static delete() {
+
+  }
+
+
+}
--- lib/flushell.dart
+++ lib/flushell.dart
@@ -1,7 +1,24 @@
 library flushell;
 
+import 'package:flushell/database/shared_preferences_table.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+
+final sh = Shell();
+
+class Shell {
+
+  Shell () {
+
+  }
+
+
+  show() {
+    print("shell ok");
+  }
 }
